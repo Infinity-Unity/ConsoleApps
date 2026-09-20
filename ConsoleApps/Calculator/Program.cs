67c371a62a6bf4edf2363c5fc3e91902b339e1bc
Abdul <thbgstudios@gmail.com>
2026-09-20 19:17:06 +0300

    update

--- ConsoleApps/Calculator/Program.cs
+++ ConsoleApps/Calculator/Program.cs
@@ -1,93 +1,115 @@
 ﻿
-/*int*/
 
 
 
-namespace Calculator
+
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        
+
+
+
+        Console.WriteLine("Добро пожаловать в приложение калькулятор!");
+        Console.WriteLine("Введите сначала два числа, а затем оператор.");
+        Console.WriteLine("При вводе чисел с плавающей запятой, введите знак в соотвествие с языком системы.");
+        Console.WriteLine("Для выхода из программы в качестве первого числа введите q");
+
+        while (true)
         {
-            string? readFirstNumber;
-            string? readSecondNumber;
-            string? readOperationSymbol;
+           
 
-            float firstNumber;
-            float secondNumber;
 
-            bool validInput = false;
-            bool validFirstRead = false;
-            bool validSecondRead = false;
+            Console.Write("Введите первое число: ");
+            string? readFirstNumber = Console.ReadLine();
 
-            Console.WriteLine("Добро пожаловать в калькулятор!\nВведите сначала два числа, затем операцию.\nВниание при введении чисел с плавающей точкой,введите запятую.");
-            do
+            if (string.IsNullOrWhiteSpace(readFirstNumber))
             {
-                Console.Write("Введите первое число: ");
-                readFirstNumber = Console.ReadLine();
-                validFirstRead = float.TryParse(readFirstNumber, out firstNumber);
-                if (!validFirstRead)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Введите корректное первое число!");
-                    continue;
-                }
+                continue;
+            }
+
+            if (readFirstNumber.ToLower() == "q")
+            {
+                Console.WriteLine("Вы вышли из программы.");
+                break;
+            }
+
+            if (!TryReadNumber(readFirstNumber, out float firstNumber))
+            {
+                Console.WriteLine("Ошибка!! Введите корректное первое число.");
+                continue;
+            }
 
 
-                Console.Write("Введите второе число: ");
-                readSecondNumber = Console.ReadLine();
-                validSecondRead = float.TryParse(readSecondNumber, out secondNumber);
+            Console.Write("Введите второе число: ");
+            string? readSecondNumber = Console.ReadLine();
+            if (!TryReadNumber(readSecondNumber, out float secondNumber))
+            {
+                Console.WriteLine("Ошибка!! Введите корректное второе число.");
+                continue;
+            }
 
-                if (!validSecondRead)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Введите корректное второе число!");
-                    continue;
-                }
+            Console.Write("Введите символ операции(+,-,*,/): ");
+            string? readOperationSymbol = Console.ReadLine();
 
-                Console.Write("Введите символ операции(+-*/^%): ");
-                readOperationSymbol = Console.ReadLine();
+            if(string.IsNullOrWhiteSpace(readOperationSymbol))
+            {
+                Console.WriteLine("Символ операции не может пустой!");
+                continue;
+            }
 
-                
+            if(TryCalculate(firstNumber, secondNumber,readOperationSymbol,out float result))
+            {
+                Console.WriteLine($"{firstNumber} {readOperationSymbol} {secondNumber} = {result}");
+            }
 
 
 
-                if(validFirstRead & validSecondRead)
-                {
-                    validInput = true;
-                }
-                else
-                {
-                    Console.WriteLine("Вы ввели данные некорректно. Попробуйте еще раз.");
-                    continue;
-                }
 
-                switch (readOperationSymbol)
-                {
-                    case "+":
-                        Console.WriteLine($"{firstNumber} {readOperationSymbol} {secondNumber} = {firstNumber + secondNumber}");
-                        break;
-                    case "-":
-                        Console.WriteLine($"{firstNumber} {readOperationSymbol} {secondNumber} = {firstNumber - secondNumber}");
-                        break;
-                    case "*":
-                        Console.WriteLine($"{firstNumber} {readOperationSymbol} {secondNumber} = {firstNumber * secondNumber}");
-                        break;
-                    case "/":
-                        Console.WriteLine($"{firstNumber} {readOperationSymbol} {secondNumber} = {firstNumber / secondNumber}");
-                        break;
-                    case "^":
-                        Console.WriteLine($"{firstNumber} {readOperationSymbol} {secondNumber} = {Math.Pow(firstNumber,secondNumber)}");
-                        break;
-                    case "%":
-                        Console.WriteLine($"{firstNumber} {readOperationSymbol} {secondNumber} = {firstNumber % secondNumber}");
-                        break;
-                }
 
-            } while (!validInput);
-            
 
-            
+
         }
+
+
+
+
+    }
+
+    static bool TryCalculate(float a, float b, string op, out float result)
+    {
+        result = 0;
+        switch (op)
+        {
+            case "+":
+                result = a + b;
+                return true;
+
+            case "-":
+                result = a - b;
+                return true;
+            case "*":
+                result = a * b;
+                return true;
+            case "/":
+                if (b == 0)
+                {
+                    Console.WriteLine("Деление на ноль!");
+                    return false;
+                }
+                result = (float)a / b;
+                return true;
+
+            default:
+                Console.WriteLine($"Команда {op} отсутствует!");
+                return false;
+        }
+        
+
+    }
+    static bool TryReadNumber(string prompt, out float result)
+    {
+        return float.TryParse(prompt, out result);
     }
 }
