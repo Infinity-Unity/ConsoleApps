bb579f8992d940a5b04859cdff104c5dc5f01171
Abdul <thbgstudios@gmail.com>
2026-09-26 13:48:01 +0300

    add basic logic

--- GuessNumber/Program.cs
+++ GuessNumber/Program.cs
@@ -1,27 +1,39 @@
 ﻿
 
+/*
+ * Классическая игра "Угадай число".
+ * 
+ * 
+ * 
+ */
+
 
 using System;
+
 class GuessNumber
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Добро пожаловать в игру угадай число!");
         Console.WriteLine("Ваша задача угадать число, который загадал компьютер.");
-        Console.WriteLine("Вы можете выбрать диапазон чисел, а также настроить другие параметры.");
+        Console.WriteLine("Вы можете выбрать диапазон чисел в настройках, а также настроить другие параметры.");
         Console.WriteLine("Нажмите:\n\n1 - Начать игру!;\n2 - Настройки;\n3 - Выйти из игры;");
 
-
+        int lowValue = 0;
+        int highValue = 10;
 
         while (true)
         {
             switch (Console.ReadLine())
             {
                 case "1":
-                    Console.WriteLine("Вы началаи игру");
+
+                    
+                    StartGame(lowValue,highValue);
+
                     break;
                 case "2":
-                    Console.WriteLine("Настройки");
+                    
                     break;
                 case "3":
                     Console.WriteLine("Вы вышли из игры.");
@@ -34,6 +46,24 @@ class GuessNumber
         }
 
         
+    }
+
+    static void StartGame(int lowValue,int highValue)
+    {
+        Console.WriteLine("Вы началаи игру");
+        Console.WriteLine($"Введите число от {lowValue} до {highValue} включительно.");
+
+        while (false)
+        {
+
+        }
+    }
+
+    static void SetSettings(ref int lowValue, ref int highValue)
+    {
+        Console.WriteLine("Настройки");
+        Console.WriteLine("Здесь вы можете изменять диапазон.");
+        Console.WriteLine($"Текущий диапазон : {lowValue} до {highValue}.");
     }
 }
 
