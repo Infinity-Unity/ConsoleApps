3ad7ab5101f6b64e9e33d86742ee4b4e5328b79e
Abdul <thbgstudios@gmail.com>
2026-09-26 13:29:07 +0300

    refactoring project & add guess number

---
+++ GuessNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+
+using System;
+class GuessNumber
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Добро пожаловать в игру угадай число!");
+        Console.WriteLine("Ваша задача угадать число, который загадал компьютер.");
+        Console.WriteLine("Вы можете выбрать диапазон чисел, а также настроить другие параметры.");
+        Console.WriteLine("Нажмите:\n\n1 - Начать игру!;\n2 - Настройки;\n3 - Выйти из игры;");
+
+
+
+        while (true)
+        {
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine("Вы началаи игру");
+                    break;
+                case "2":
+                    Console.WriteLine("Настройки");
+                    break;
+                case "3":
+                    Console.WriteLine("Вы вышли из игры.");
+                    return;
+                default:
+                    Console.WriteLine("Такой опции не существует.");
+                    break;
+
+            }
+        }
+
+        
+    }
+}
+
