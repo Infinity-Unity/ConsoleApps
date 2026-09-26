4da7893a4d4bc177baa205577fb1e54c0837c3d4
Abdul <thbgstudios@gmail.com>
2026-09-26 15:33:03 +0300

    update to work game

--- GuessNumber/Program.cs
+++ GuessNumber/Program.cs
@@ -14,10 +14,8 @@ class GuessNumber
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Добро пожаловать в игру угадай число!");
-        Console.WriteLine("Ваша задача угадать число, который загадал компьютер.");
-        Console.WriteLine("Вы можете выбрать диапазон чисел в настройках, а также настроить другие параметры.");
-        Console.WriteLine("Нажмите:\n\n1 - Начать игру!;\n2 - Настройки;\n3 - Выйти из игры;");
+        
+        ShowInfo();
 
         int lowValue = 0;
         int highValue = 10;
@@ -27,10 +25,7 @@ class GuessNumber
             switch (Console.ReadLine())
             {
                 case "1":
-
-                    
                     StartGame(lowValue,highValue);
-
                     break;
                 case "2":
                     SetSettings(ref lowValue,ref highValue);
@@ -53,9 +48,18 @@ class GuessNumber
         Console.WriteLine("Вы началаи игру");
         Console.WriteLine($"Введите число от {lowValue} до {highValue} включительно.");
 
-        while (false)
-        {
+        Random random = new Random();
 
+        int randomLowValue = random.Next(lowValue);
+        int randomHighValue = random.Next(highValue);
+
+        int secretNumber = random.Next(randomLowValue,randomHighValue + 1);
+
+        while (true)
+        {
+            Console.WriteLine($"Загадайте число от {randomLowValue} до {randomHighValue}");
+            Console.WriteLine(secretNumber);
+            if (Console.ReadLine() == "q") break;
         }
     }
 
@@ -95,13 +99,14 @@ class GuessNumber
                     continue;
                 }
 
-                Console.WriteLine($"Отлично! Ввод изменен с [{lowValue} : {highValue}] на [{lowNumber} : {highNumber}].");
+                Console.WriteLine($"Отлично! Диапазон изменен с [{lowValue} : {highValue}] на [{lowNumber} : {highNumber}].");
                 lowValue = lowNumber;
                 highValue = highNumber;
                 break;
             }
             else if(readUserConfirm.ToLower().Trim() == "n")
             {
+                Console.WriteLine("Вы вышли из настроек.");
                 break;
             }
         }
@@ -110,5 +115,35 @@ class GuessNumber
         int secondOption = 0;
 
     }
+
+    static void ShowInfo()
+    {
+        Console.SetCursorPosition(10, 10);
+        int widthConsole = Console.WindowWidth - 80;
+        int heightConsole = Console.WindowHeight - 10;
+
+
+
+
+        Console.SetCursorPosition(widthConsole / 2, heightConsole / 2);
+        Console.WriteLine("Добро пожаловать в игру угадай число!");
+
+        Console.SetCursorPosition(widthConsole / 2, (heightConsole / 2) + 1);
+        Console.WriteLine("Ваша задача угадать число, который загадал компьютер.");
+
+        Console.SetCursorPosition(widthConsole / 2, (heightConsole / 2) + 2);
+        Console.WriteLine("Вы можете выбрать диапазон чисел в настройках, а также настроить другие параметры.");
+
+        Console.SetCursorPosition(widthConsole / 2, (heightConsole / 2) + 2);
+        Console.WriteLine("Нажмите любую кнопку чтобы продолжить");
+        Console.ReadKey();
+
+        Console.SetCursorPosition(0, 0);
+        Console.Clear();
+
+        Console.WriteLine("Нажмите:\n\n1 - Начать игру!;\n2 - Настройки;\n3 - Выйти из игры;");
+    }
+
+    
 }
 
